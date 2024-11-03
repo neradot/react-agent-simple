514ba3af20e48e5c2d91a5773b75f3a638179339
royp-nrd <roy@neradot.com>
2024-11-03 11:41:52 +0200

    print prompts

--- prompts.py
+++ prompts.py
@@ -2,11 +2,13 @@ REACT_TOOLS_DESCRIPTION = """The available tools are:
 {tools_description}
 
 Use the tool name as the "action" value in the JSON response.
+
 """
 
 REACT_VALID_ACTIONS = """Valid actions are:
 - "Final Answer"
-- Any of {tools_names}"""
+- Any of {tools_names}
+"""
 
 REACT_JSON_FORMAT = """
 Respond in the following $JSON_BLOB format:
@@ -22,6 +24,7 @@ Respond in the following $JSON_BLOB format:
 REACT_PROCESS_FORMAT = """
 After you select an action, you will receive an observation. Then you can select another action or provide a final answer.
 The pattern looks like this:
+
 <reasoning>
 $JSON_BLOB
 </reasoning>
@@ -34,7 +37,9 @@ $JSON_BLOB
 <observation>
 {"observation": // action result}
 </observation>
+
 ... (repeated until you have enough observations to answer the question)
+
 <reasoning>
 {
         "thought": // Explain why you have enough information to provide a final answer,
@@ -44,14 +49,27 @@ $JSON_BLOB
 </reasoning>
 """
 
-REACT_ADDITIONAL_INSTRUCTIONS = """Instructions:
+REACT_ADDITIONAL_INSTRUCTIONS = """
+Instructions:
 1. Do not use comments in your JSON answer;
 2. ALWAYS respond with a valid json blob of a single action;
 3. ALWAYS think before choosing an action;
 4. Respond in a JSON blob no matter what."""
 
-REACT_INTERMEDIATE_STEPS = """Question: "{question}"
+REACT_INTERMEDIATE_STEPS = """
 
+Here is the user question: 
+"{question}"
+
+Here are the intermediate steps so far:
 {intermediate_steps}
 """
 
+if __name__ == "__main__":
+        print(REACT_TOOLS_DESCRIPTION + \
+                REACT_VALID_ACTIONS + \
+                REACT_JSON_FORMAT + \
+                REACT_PROCESS_FORMAT + \
+                REACT_ADDITIONAL_INSTRUCTIONS + \
+                REACT_INTERMEDIATE_STEPS
+                )
